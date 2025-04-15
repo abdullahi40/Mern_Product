52b8aa8a2f87cdbc0cafb369e59af1f3f0c18841
Abdullai-Suleiman <146683810+abdullahi40@users.noreply.github.com>
2025-04-15 11:22:45 +0300

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,13 +7,9 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-<<<<<<< HEAD
-        target: "https://mern-product-6ygk.onrender.com",
-=======
         target: "https://mern-product-backend-rho.vercel.app",
         changeOrigin: true,
         secure: false,
->>>>>>> c0bd42c9797da19d41b87e022e9bd64413ecc0b8
       },
     },
   },
